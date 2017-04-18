3fd93ab1b971c93b98ec29b3a26db0b2e0c9ace4
Pavlos <pavlos@edu7F70.kent.ac.uk>
2017-04-18 22:44:51 +0100

    created the UI of About me page

--- app/src/main/java/pavlosnicolaou/newsapp/AboutMeActivity.java
+++ app/src/main/java/pavlosnicolaou/newsapp/AboutMeActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class AboutMeActivity extends AppCompatActivity {
 
+    private
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
